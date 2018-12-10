bf7833b130044fc896ab4b800bf7a73005fbf813
Terry Moore <tmm@mcci.com>
2018-12-10 00:07:49 -0500

    Remove yosys inferred latches

--- common/usb_fs_in_arb.v
+++ common/usb_fs_in_arb.v
@@ -20,18 +20,16 @@ module usb_fs_in_arb #(
   always @* begin
     grant = 0;
 
+    arb_in_ep_data = 0;
+
     for (i = 0; i < NUM_IN_EPS; i = i + 1) begin
-      in_ep_grant[i] <= 0;
+      in_ep_grant[i] = 0;
 
       if (in_ep_req[i] && !grant) begin
-        in_ep_grant[i] <= 1;
-        arb_in_ep_data <= in_ep_data[i * 8 +: 8];
+        in_ep_grant[i] = 1;
+        arb_in_ep_data = in_ep_data[i * 8 +: 8];
         grant = 1;
       end
-    end
-
-    if (!grant) begin
-      arb_in_ep_data <= 0;
     end
   end
 endmodule
