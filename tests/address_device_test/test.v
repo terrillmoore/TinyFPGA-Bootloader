b4421b41bc63df73532a54bbf0dd950c377568b3
Terry Moore <tmm@mcci.com>
2018-12-17 00:46:27 -0500

    remove structural assert for now

--- tests/address_device_test/test.v
+++ tests/address_device_test/test.v
@@ -1,7 +1,11 @@
 `include "top_tb_header.vh"
   initial begin
+    // TODO(tmm@mcci.com) we shoud send a USB reset; and we ought to
+    // probe the address some other way, rather than trying to look
+    // in the registers -- that only works for functional, not timing,
+    // verification.
     send_usb_address_device(8'h00, 8'h1e);
-    `assert("new device address", dut.dev_addr, 7'h1e);
+    //`assert("new device address", `DUT.dev_addr, 7'h1e);
 
     $finish(0);
   end
